1e3ba1b0eadd739be3cb8021f7fa3a3379e13742
Stephanie Briones <smbriones@gmail.com>
2016-06-06 20:36:28 -0500

    Adds SCSS support using node-sass-middleware

--- server.babel.js
+++ server.babel.js
@@ -1,7 +1,14 @@
 import express from 'express';
-
+const sassMiddleware = require('node-sass-middleware');
+const path = require('path');
 const app = express();
 
-app.use('/', express.static('public'));
-
 app.listen(process.env.PORT || 3000);
+
+app.use(sassMiddleware({
+  src: __dirname + '/sass',
+    dest: __dirname + '/public/stylesheets',
+    debug: true,
+  })
+);
+app.use(express.static(path.join(__dirname, 'public')));
